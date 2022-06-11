d829ba6a3b155d4da6eb65da38586064d293528c
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-06-11 09:43:56 +0300

    This will add the Value event listener methods

--- app/src/main/java/com/moringaschool/thenewsapi/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/thenewsapi/ui/MainActivity.java
@@ -1,16 +1,21 @@
 package com.moringaschool.thenewsapi.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.moringaschool.thenewsapi.Constants;
 import com.moringaschool.thenewsapi.R;
 
@@ -31,7 +36,25 @@ public class MainActivity extends AppCompatActivity {
         mSeatrchedNewsReference = FirebaseDatabase
                 .getInstance()
                         .getReference()
-                                .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
+                                .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION); //Points to news code
+
+        //Adding the value event Listener methods
+        mSeatrchedNewsReference.addValueEventListener(new ValueEventListener() {
+            @Override
+
+            //Changing the data in the node
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot newsSnapshot : snapshot.getChildren()){
+                    String news = newsSnapshot.getValue().toString();
+                    Log.d("news updated", "news: " + news);
+                }
+            }
+            //called when the listener is unsuccesful
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
         super.onCreate(savedInstanceState);
@@ -41,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //saving news when the button is clicked
                 if(v == mButton){
                     String news = mCategory.getText().toString();
                     saveNewsToFirebase(news);
