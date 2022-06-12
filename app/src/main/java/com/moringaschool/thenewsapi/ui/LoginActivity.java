0089f5eba7bfdab1e79bad51b431165812328d6d
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-06-12 14:49:02 +0300

    This will add the intent to move the the create account activity

--- app/src/main/java/com/moringaschool/thenewsapi/ui/LoginActivity.java
+++ app/src/main/java/com/moringaschool/thenewsapi/ui/LoginActivity.java
@@ -2,15 +2,35 @@ package com.moringaschool.thenewsapi.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.moringaschool.thenewsapi.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.registerTextView) TextView mregisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mregisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mregisterTextView){
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 }
