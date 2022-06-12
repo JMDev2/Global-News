c426c2eeb254e9328a35b91eb27e30e2cbbb86bf
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-06-12 15:14:07 +0300

    This will bind the create account variables and the intents

--- app/src/main/java/com/moringaschool/thenewsapi/ui/CreateAccountActivity.java
+++ app/src/main/java/com/moringaschool/thenewsapi/ui/CreateAccountActivity.java
@@ -2,15 +2,47 @@ package com.moringaschool.thenewsapi.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.moringaschool.thenewsapi.R;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.logInTextView) TextView mLoginTextView;
+    @BindView(R.id.emailEditText) EditText mEmail;
+    @BindView(R.id.nameEditText) EditText mName;
+    @BindView(R.id.passwordEditText) EditText mPassword;
+    @BindView(R.id.confirmPasswordEditText) EditText mConfirmPassword;
+    @BindView(R.id.signUpBtn) Button mRegisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        ButterKnife.bind(this);
+
+        mLoginTextView.setOnClickListener(this);
+        mRegisterButton.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mLoginTextView){
+            Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+
+        }
     }
 }
